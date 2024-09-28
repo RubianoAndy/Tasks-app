0eb7dd2d9b264dd063404a2c89929aa3b57d29f9
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-09-28 17:31:39 -0500

    feat: ading table schema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./components/tasks/tasks.component') },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];

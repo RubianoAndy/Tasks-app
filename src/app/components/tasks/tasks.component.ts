04a3945de053b60141d0d0c7a484334b8606b223
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-09-29 09:46:41 -0500

    feat: adding delete button event clic

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AlertService } from '../../global/services/alert/alert.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,7 +17,29 @@ import { RouterLink } from '@angular/router';
 export default class TasksComponent implements OnInit {
   totalTasks: any[] = [];
 
+  constructor(
+    private alertService: AlertService,
+  ) { }
+
   ngOnInit(): void {
     this.totalTasks = localStorage.getItem('Tareas') ? JSON.parse(localStorage.getItem('Tareas')!) : [];
+  }
+
+  deleteTask(id: number) {
+    var tasks = localStorage.getItem('Tareas') ? JSON.parse(localStorage.getItem('Tareas')!) : [];
+    tasks = tasks.filter((task: any) => task.id !== id);
+    localStorage.setItem('Tareas', JSON.stringify(tasks));
+
+    var alertBody = null;
+
+    alertBody = {
+      type: 'okay',
+      title: '¡Felicitaciones!',
+      message: 'La tarea se ha borrado exitosamente',
+    };
+
+    this.alertService.showAlert(alertBody);
+
+    this.ngOnInit();
   }
 }
